f77fabd4ad137ea43046eee0415c79a545fd52c0
Anton Bilchuck <anton@onsweb.nl>
2020-06-19 18:30:53 +0300

    fix: deploy file

--- deploy/deploy_prd.sh
+++ deploy/deploy_prd.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+set -e;
+
+cd /home/link-shortener
+
 git pull
 docker-compose -p link-shortener -f docker-compose.prd.yml stop
 docker-compose -p link-shortener -f docker-compose.prd.yml up -d
